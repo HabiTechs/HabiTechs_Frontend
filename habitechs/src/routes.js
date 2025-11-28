d1d8b5030ed452549496f6882a0111e14756f056
gabiqaa <gabiqaa@users.noreply.github.com>
2025-11-28 16:54:59 -0400

    PR11: agregar archivo de configuración de rutas internas

---
+++ habitechs/src/routes.js
@@ -0,0 +1,12 @@
+// PR11: Configuración de rutas internas de Habitex
+
+const routes = {
+    home: "/",
+    dashboard: "/dashboard",
+    residents: "/residents",
+    security: "/security",
+    payments: "/payments",
+  };
+  
+  export default routes;
+  
