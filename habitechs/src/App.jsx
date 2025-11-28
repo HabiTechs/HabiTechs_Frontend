e8e9a7ab156dda1608b478aea968c47674cebd0f
gabiqaa <gabiqaa@users.noreply.github.com>
2025-11-28 16:51:12 -0400

    PR7: agregar logs de depuración en App.jsx

---
+++ habitechs/src/App.jsx
@@ -0,0 +1,8 @@
+// PR4: Comentario de prueba para Pull Request — Habitex
+import React from "react";
+
+// PR7: Logs de depuración para Habitex
+export default function App() {
+    console.log("PR7: App.jsx cargado correctamente");
+    return <div>Componente App</div>;
+}
